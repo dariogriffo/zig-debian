8cc80886df3dbb354555646fa053b739e358dac6
Dario Griffo <dariogriffo@gmail.com>
2025-07-24 13:15:00 +0100

    Update build

--- build_zig_debian.sh
+++ build_zig_debian.sh
@@ -8,13 +8,12 @@ do
   
   docker build . -t zig-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION -f meta_Dockerfile
   id="$(docker create zig-$DEBIAN_DIST)"
-  docker cp $id:/zig.deb - > ./zig.deb
-  tar -xf ./zig.deb
-
-  docker build . -t zig-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION -f zero_Dockerfile
-  id="$(docker create zig-$DEBIAN_DIST)"
   docker cp $id:/zig_$FULL_VERSION.deb - > ./zig_$FULL_VERSION.deb
   tar -xf ./zig_$FULL_VERSION.deb
 
+  docker build . -t zig-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION -f zero_Dockerfile
+  id="$(docker create zig-$DEBIAN_DIST)"
+  docker cp $id:/zig-zero_$FULL_VERSION.deb - > ./zig-zero_$FULL_VERSION.deb
+  tar -xf ./zig-zero_$FULL_VERSION.deb
 
 done
